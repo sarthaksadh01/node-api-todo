0921bc94295d5ad73354e2c1fa523c0d850943fe
Sarthak Sadh <sarthaksadh@Sarthaks-MacBook-Pro.local>
2018-12-18 00:55:34 +0530

    mongo updated

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise =global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect("mongodb://sarthak01:Sarthak01@@ds237574.mlab.com:37574/stodos" || 'mongodb://localhost:27017/TodoApp');
 module.exports={
     mongoose
 }
