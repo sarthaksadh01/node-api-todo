e0d653a7007e2f3fa8fa899cb8888e3cf90a421b
Sarthak Sadh <sarthaksadh@Sarthaks-MacBook-Pro.local>
2018-12-18 00:04:26 +0530

    api using node

---
+++ server/server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var User = require('./models/users');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/get', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+    console.log('server started');
+});
+
+
